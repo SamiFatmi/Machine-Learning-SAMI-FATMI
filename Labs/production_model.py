43523799d02e0da46a53bcabb0ebe8643770a705
SamiFatmi <89996292+SamiFatmi@users.noreply.github.com>
2022-03-26 21:51:42 +0100

    Minor changes

--- Labs/production_model.py
+++ Labs/production_model.py
@@ -1,9 +1,27 @@
 import pandas as pd
 import joblib  as jl
+from sklearn.ensemble import VotingClassifier
 
 # 1. Read in the tests samples
-df_test = pd.read_csv("test_samples.csv", index_col = 0)
+test_df = pd.read_csv("test_samples.csv", index_col = 0)
 
 # 2. Read in the trained model
-classifier = jl.load("trained_model.pkl")
+model = jl.load("trained_model.pkl")
 
+# 3. Splitting the data into X and y
+X = test_df.drop(["id","cardio"], axis = 1)
+y = test_df["cardio"]
+
+# 4. Making predictions and prediction probabilities
+y_pred = model.predict(X)
+y_probabilities = model.predict_proba(X)
+
+# 5. Creating the dataframe 
+predictions_df = pd.DataFrame({
+                                "Probability Class 0": y_probabilities[:,0],
+                                "Probability Class 1": y_probabilities[:,1],
+                                "Predicted": y_pred
+                            })
+
+# 6. Exporting the dataframe as csv
+predictions_df.to_csv("prediction.csv")
