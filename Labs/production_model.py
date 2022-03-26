179fd036ea975c151423d0ab796d8ed3476ceb45
SamiFatmi <89996292+SamiFatmi@users.noreply.github.com>
2022-03-26 21:29:58 +0100

    Loading the trained model

--- Labs/production_model.py
+++ Labs/production_model.py
@@ -1,5 +1,9 @@
 import pandas as pd
+import joblib  as jl
 
 # 1. Read in the tests samples
 df_test = pd.read_csv("test_samples.csv", index_col = 0)
+
+# 2. Read in the trained model
+classifier = jl.load("trained_model.pkl")
 
